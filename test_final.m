7800e1b15cfa0723957c57f62a04402e3dffc339
varunsan <varunsanghvi2000@gmail.com>
2017-10-18 12:21:22 -0400

    test_final_check

--- test_final.m
+++ test_final.m
@@ -1,15 +1,20 @@
 qstart=[0,1.4,1,1,1,10];
-qsend=[0.5,-1.4,1.2,0,0.2,10];
+qsend=[1,-1.4,1.2,0,0.2,10];
 pathplan(qstart,qsend);
 
 function pathplan(qstart,qend)
 qstarttree =qstart;
 qendtree=qend;
-pathcheck=checklinecol(qstart,qend);
+pathcheck=checklinecol(qstarttree,qendtree);
 if pathcheck == 1           %colides
    q=randomq();
    addpointtolist(qstarttree,qendtree,q)
+else
+    lynxServo(qstart);
+    
+    lynxServo(qend);
 end
+
 end
 
 function q=randomq()
@@ -21,27 +26,38 @@ q5=-1.4+(1.4+1.4)*rand;
 q=[q1,q2,q3,q4,q5];
 end
 
-function cspacepoints=addpointtolist(qstarttree,qendtree,q)
+function [qstarttree,qendtree]=addpointtolist(qstarttree,qendtree,q)
 [lenqst mstrt]=size(qstarttree);
 [lenen mend]=size(qendtree);
 
+dist_index1=zeros(lenqst,2);
+dist_index2=zeros(lenen,2);
 for n=1:lenqst
-    qint=qstarttree(n:1);
-    flag=checklinecol(qint,q);
-    if flag==0
-    dist=distcheck(qstarttree(n,:),q);
-    dist_index1=vertcat(dist_index,[dist,n]);
+    qintstart=qstarttree(n,:);
+    flagstart=checklinecol(qintstart,q);
+    if flagstart==0
+    dist=distcheck(qintstart,q);
+    dist_index1=vertcat(dist_index1,[dist,n]);
     end
 end
-B = sortrows1(dist_index);
-for n=1:lenqst
-    flag=checklinecol(qstarttree,q);
-    if flag==0
-    dist=distcheck(qstarttree(n,:),q);
-    dist_index2=[dist,index];
+dist1 = sortrows(dist_index1);
+for n=1:lenen
+    qintend=qendtree(n,:);
+    flagend=checklinecol(qintend,q);
+    if flagend==0
+    dist=distcheck(qintend,q);
+    dist_index2=vertcat(dist_index2,[dist,n]);
     end
 end
-B = sortrows(dist_index);
+dist2 = sortrows(dist_index2);
+if dist1(2,1)<dist2(2,1)
+    qendtree=vertcat(qendtree,q);
+    %add edge
+else
+    qstarttree=vertcat(qstarttree,q);
+    %add edge
+end
+    
 
 % % flag=checklinecol(qendtree,q);
 % % for n=1:len
@@ -90,11 +106,12 @@ stepq5=linspace(q1(5),q2(5),discretization);
 end
 
 function F=spherecol(coodr)
+sphere_size=20;
 x=coodr(1,1);
 y=coodr(1,2);
 z=coodr(1,3);
 F=0;
-if(((x-60)^2+(y+300)^2+(z-170)^2)<80^2 || ((x-60)^2+(y-100)^2+(z-150)^2)<80^2 || ((x-210)^2+(y+200)^2+(z-120)^2)<80^2 || ((x-210)^2+(y-100)^2+(z-150)^2)<80^2)
+if(((x-60)^2+(y+300)^2+(z-170)^2)<sphere_size^2 || ((x-60)^2+(y-100)^2+(z-150)^2)<sphere_size^2 || ((x-210)^2+(y+200)^2+(z-120)^2)<sphere_size^2 || ((x-210)^2+(y-100)^2+(z-150)^2)<sphere_size^2)
     F=1;
 end
 end
